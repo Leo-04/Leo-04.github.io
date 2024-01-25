ff7ac06b5a4d8184473d2e28416f9eb51c2ec13a
Leo-04 <50501140+Leo-04@users.noreply.github.com>
2024-01-25 17:17:25 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -8,11 +8,18 @@ function Save(){
 	
 }
 function Menu(){
-	console.log(menu.style.display)
 	if (menu.style.display == "none" || menu.style.display == ""){
 		menu.style.display = "block";
 	} else {
 		menu.style.display = "none";
+		resize.style.display = "none";
+	}
+}
+function Resize(){
+	if (resize.style.display == "none" || resize.style.display == ""){
+		resize.style.display = "block";
+	} else {
+		resize.style.display = "none";
 	}
 }
 function Undo(){
@@ -21,6 +28,10 @@ function Undo(){
 function Redo(){
 	
 }
+function DoResize(){
+	
+}
+
 function C1(){
 	selected_color = c1.style.backgroundColor;
 	c1.style.border = "10px solid red";
@@ -42,19 +53,24 @@ function C3(){
 function ColorPicker(e){
 	
 }
-function Resize(){
-	
-}
 function Export(){
 	
 }
 
-var c1, c2, c3, canvas, menu, grid, major_grid, grid_color, major_color, bg_color;
+var c1, c2, c3, canvas, menu, grid, major_grid, grid_color, major_color, bg_color, resize;
+var ctx;
 var selected_color;
+var width=8;
+var height=8;
+var pixels=[]
+for (var i=0; i<64; i++){
+	pixels.push("#FFFFFFFF");
+}
 
 window.onload = function(){
 	canvas = document.getElementById("canvas");
 	menu = document.getElementById("Menu");
+	resize = document.getElementById("Resize");
 	c1 = document.getElementById("c1");
 	c2 = document.getElementById("c2");
 	c3 = document.getElementById("c3");
@@ -64,5 +80,49 @@ window.onload = function(){
 	grid_color = document.getElementById("grid_color");
 	major_color = document.getElementById("major_color");
 	bg_color = document.getElementById("bg_color");
+	ctx = canvas.getContext("2d");
 	C1()
+	
+	setInterval(Draw, 100);
+}
+window.onresize = function(){
+	ctx.canvas.width = canvas.clientWidth;
+    ctx.canvas.height = canvas.clientHeight;
+}
+
+function Press(){
+	
+}
+function Drag(){
+	
+}
+function Release(){
+	
+}
+
+
+function Draw(){
+	var grid_size=parseInt(major.value);
+	
+	if (!grid.checked){
+		grid_size = 0;
+	}
+	var pixel_size = Math.floor(Math.min(
+		canvas.clientWidth/(grid_size+width),
+		canvas.clientHeight/(grid_size+height)
+	));
+	var start_x = Math.floor((canvas.clientWidth-(pixel_size*width))/2);
+	var start_y = Math.floor((canvas.clientHeight-(pixel_size*height))/2);
+	
+	ctx.fillStyle = bg_color.style.backgroundColor;
+	ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+	for (var x=0; x<width; x++){
+		for (var y=0; y<height; y++){
+			ctx.fillStyle = grid_color.style.backgroundColor;
+			ctx.fillRect(start_x + pixel_size*x, start_y + pixel_size*y, pixel_size, pixel_size);
+			ctx.fillStyle = pixels[y*width + x];
+			ctx.fillRect(start_x + pixel_size*x + grid_size, start_y + pixel_size*y + grid_size, pixel_size - grid_size, pixel_size - grid_size);
+		}
+	}
+	ctx.fill();
 }
